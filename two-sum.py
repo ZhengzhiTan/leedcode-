42867377c5c391bd9d679f0ec8baf82a401acf16
ZhengzhiTan <66708716+ZhengzhiTan@users.noreply.github.com>
2022-07-18 01:05:55 +0800

    Create two-sum.py

---
+++ two-sum.py
@@ -0,0 +1,34 @@
+##method 1 two loop
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for index1,num in enumerate(nums):
+            for index2 in range(index1+1,len(nums)):
+                if num + nums[index2] == target:
+                    return [index1,index2]
+## method 2 two pivots
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left = 0 
+        right = len(nums)-1
+        nums_sorted = sorted(nums)
+        print(nums)
+        while left < right:
+
+            if nums_sorted[left] + nums_sorted[right] == target:
+                print(nums)
+                nums_leftindex = nums.index(nums_sorted[left])
+                nums_rightindex =nums.index(nums_sorted[right])
+                if nums_leftindex == nums_rightindex:
+                    nums_rightindex =nums.index(nums_sorted[right],nums.index(nums_sorted[left])+1)
+                    return [nums_leftindex,nums_rightindex]
+                else:
+                    return [nums_leftindex,nums_rightindex]
+
+                
+            elif nums_sorted[left] + nums_sorted[right] > target:
+                right = right - 1
+            else:
+                left = left + 1
+
+        return [-1]
+## hash map/ dict
